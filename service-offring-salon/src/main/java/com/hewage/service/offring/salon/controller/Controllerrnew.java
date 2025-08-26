ee99de8618d1156a01c5ac2016aab5b40e63a2dd
Pasindu991182 <pasindughp@gmail.com>
2025-08-26 12:03:51 +0530

    complte first service commit

--- service-offring-salon/src/main/java/com/hewage/service/offring/salon/controller/Controllerrnew.java
+++
@@ -1,7 +0,0 @@
-package com.hewage.service.offring.salon.controller;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class Controllerrnew {
-}
--- service-offring-salon/src/main/java/com/hewage/service/offring/salon/controller/SalonServiceOfferingController.java
+++ service-offring-salon/src/main/java/com/hewage/service/offring/salon/controller/SalonServiceOfferingController.java
@@ -1,41 +1,7 @@
 package com.hewage.service.offring.salon.controller;
 
-import com.hewage.service.offring.salon.model.ServiceOffering;
-import com.hewage.service.offring.salon.services.ServicesOfferingService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.Set;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequiredArgsConstructor
-@RequestMapping("/api/service-offering")
 public class SalonServiceOfferingController {
-
-    private final ServicesOfferingService servicesOfferingService;
-
-    @GetMapping("/salon/{salonId}")
-    public ResponseEntity<Set<ServiceOffering>> getServiceBySalonId(
-            @PathVariable Long salonId,
-            @RequestParam(required = false) Long category
-    ) {
-        Set<ServiceOffering> serviceOfferings = servicesOfferingService.GetAllServiceBySalonId(salonId, category);
-        return ResponseEntity.ok(serviceOfferings);
-    }
-
-    @GetMapping("/{id}")
-    public ResponseEntity<ServiceOffering> getServiceById(
-            @PathVariable Long id) throws Exception {
-        ServiceOffering serviceOffering = servicesOfferingService.getServiceById(id);
-        return ResponseEntity.ok(serviceOffering);
-    }
-
-    @GetMapping("/list/{ids}")
-    public ResponseEntity<Set<ServiceOffering>> getServiceBySalonIds(
-            @PathVariable  Set<Long> ids
-    ){
-        Set<ServiceOffering> serviceOfferings = servicesOfferingService.getServiceByIds(ids);
-        return ResponseEntity.ok(serviceOfferings);
-    }
 }
